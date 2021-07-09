768d1b84cce1067b33c3bb0c9df74c91753afc1f
huangshuen <huangshuen@viomi.com>
2021-07-09 18:45:22 +0800

    测试毫不相干的代码的执行顺序

--- src/html/1/2.js
+++ src/html/1/2.js
@@ -4,3 +4,21 @@
  * 但是ABCD接口调用Z的结果，如果Z的结果过期之类的，就和后台获取新的结果，后面的请求都要挂起来等待结果
  * ABCD是用户主动点击触发的，时间不定
  */
+function a1(params) {
+    new Promise(o => {
+        setTimeout(() => {
+            console.log("a1")
+            o()
+        }, 1000);
+    })
+}
+
+function a2(params) {
+    console.log('====================================');
+    console.log("a2");
+    console.log('====================================');
+}
+a1()
+setTimeout(() => {
+    a2()
+}, 1200);
