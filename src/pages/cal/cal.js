721a8dae871a64da97be26a21d20f16bc9ca0b73
huangshuen <huangshuen@viomi.com>
2021-07-27 14:55:11 +0800

    修改

---
+++ src/pages/cal/cal.js
@@ -0,0 +1,18 @@
+function sunBankruptcy (num=0,day=0,rate=0){
+    return num*day*rate
+}
+import React from 'react'
+
+export default class cal extends React.Component {
+    constructor(){
+        this.test=1
+    }
+    render() {
+        return (
+            <div>
+                {this.test}
+            </div>
+        )
+    }
+}
+
